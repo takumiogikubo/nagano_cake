1e42603d9254086332c6579153a5e5c2902453d9
takumiogikubo <104717759+takumiogikubo@users.noreply.github.com>
2022-05-22 07:28:24 +0000

    [Update]顧客側のサインイン・サインアップ、管理者側のサインイン画面（views）の編集

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "0da8b805acea4c7393369df36aa3fbc6.vfs.cloud9.us-east-1.amazonaws.com"
 end
