e2c600e6d8ca00669e99c395658a1f066ca6113f
takumiogikubo <104717759+takumiogikubo@users.noreply.github.com>
2022-05-22 11:27:17 +0000

    [Update]order_detailsのマイグレーション完了

--- db/migrate/20220522085811_create_items.rb
+++ db/migrate/20220522085811_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
 
-      t.references :genre, null:t.false, foreign_key: true
+      t.references :genre, null:false, foreign_key: true
       t.string :name, null:false, defalt: ""
       t.text :introduction, null:false, defalt: ""
       t.integer :price, null:false, defalt: ""
---
+++ db/migrate/20220522112153_create_order_details.rb
@@ -0,0 +1,14 @@
+class CreateOrderDetails < ActiveRecord::Migration[6.1]
+  def change
+    create_table :order_details do |t|
+
+      t.references :order, null:false, foreign_key: true
+      t.references :item, null:false, foreign_key: true
+      t.integer:price, null:false, defalt: ""
+      t.integer:amount, null:false, defalt: ""
+      t.integer:makig_status, null:false, defalt: "0"
+
+      t.timestamps
+    end
+  end
+end
