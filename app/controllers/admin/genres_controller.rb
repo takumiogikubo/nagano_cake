26e1953455f2e526a783c09a89c2ee75ff909539
takumiogikubo <104717759+takumiogikubo@users.noreply.github.com>
2022-05-22 12:17:22 +0000

    [Update]admin内のコントローラーのアクションを修正

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -2,6 +2,12 @@ class Admin::GenresController < ApplicationController
   def index
   end
 
+  def create
+  end
+
   def edit
+  end
+
+  def update
   end
 end
--- app/controllers/admin/order_derails_controller.rb
+++ app/controllers/admin/order_derails_controller.rb
@@ -1,2 +1,6 @@
 class Admin::OrderDerailsController < ApplicationController
+
+  def update
+  end
+
 end
