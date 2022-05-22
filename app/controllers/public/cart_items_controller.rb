4cc389261843b7d40a3d1ef7a2f5934bc525be94
takumiogikubo <104717759+takumiogikubo@users.noreply.github.com>
2022-05-22 13:19:12 +0000

    [update]1:Nの関連付けを実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,5 @@
 class Public::CartItemsController < ApplicationController
+
   def index
   end
 
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,6 +3,8 @@
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  before_action :customer_state, only:[:create]
+
   # GET /resource/sign_in
   # def new
   #   super
@@ -24,4 +26,19 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  protected
+  # 退会しているかを判断するメソッド
+  def customer_state
+    ## 【処理内容1】 入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+    ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@customer
+    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @customer.valid_password?(params[:customer][:passwprd]) && !@customer.is_valid
+      redirect_to new_customer_session_path
+    end
+  end
+
+
 end
