f6330bbb92049fe8cd21f45e33fc8b6ae78bd074
takumiogikubo <104717759+takumiogikubo@users.noreply.github.com>
2022-05-28 15:39:12 +0000

    [Update]ヘッダーとフッダー及びadminのログアウト後の遷移先指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def after_sign_out_path_for(resource_or_scope)
+    return new_admin_session_path if resource_or_scope == :admin
+    root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
 
+# devise_for :customers
+  # 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# devise_for :admins
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
 
   scope module: 'public' do
     root to: "homes#top"
@@ -38,20 +52,6 @@ Rails.application.routes.draw do
     resources :order_details, only:[:update]
   end
 
-# devise_for :customers
-  # 顧客用
-# URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-
-# devise_for :admins
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
 
 
 
