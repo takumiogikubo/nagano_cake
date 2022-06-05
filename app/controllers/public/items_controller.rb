fa09488adfeec8e455142b1e9069e9a12de49de6
takumiogikubo <104717759+takumiogikubo@users.noreply.github.com>
2022-06-05 14:16:19 +0000

    [Update]publicの商品一覧機能完了

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,5 +1,9 @@
 class Public::ItemsController < ApplicationController
   def index
+    @genres=Genre.all
+    @item=Item.find_by(genre_id: params[:genre_id])
+    @items=Item.where(genre_id: params[:genre_id]).page(params[:page])
+
   end
 
   def show
