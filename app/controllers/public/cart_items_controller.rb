cebd5875c631422b9612b0f6c1576edcc3c5fb42
takumiogikubo <104717759+takumiogikubo@users.noreply.github.com>
2022-06-09 08:14:10 +0000

    [Uodate]商品詳細からカートへ商品と個数を入れて表示Edit.deleteも実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,20 +1,33 @@
 class Public::CartItemsController < ApplicationController
 
   def index
+    @cart_items=current_customer.cart_items.all
+    # @items=I/tem.all
   end
 
   def update
+    cart_item = CartItem.find(params[:id])
+    cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
   def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to cart_items_path
   end
 
   def destroy_all
   end
 
   def create
-     @item = Item.find(cart_item_params[:item_id])
-     redirect_to cart_items_path
+    @cart_item=current_customer.cart_items.new(cart_item_params)
+    @cart_item.save
+    redirect_to cart_items_path
+
+    # @item = Item.find(cart_item_params[:item_id])
+    # @item.save
+    # redirect_to cart_items_path
   end
 
   private
