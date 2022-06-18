37326955e503706d895930ed4972ca4370130e62
takumiogikubo <104717759+takumiogikubo@users.noreply.github.com>
2022-06-18 12:18:56 +0000

    {update]注文詳細画面完了

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,7 +3,7 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail=OrderDetail.find(params[:id])
     @order_detail.update(order_detail_params)
-    redirect_to admin_order_path(params[:id])
+    redirect_back fallback_location: { action: "show", id: :id }
   end
 
   # def create
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,6 +2,6 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
-  enum makig_status:{not_work:0,wait_work:1,work:2,complete_work:3}
+  enum making_status:{not_work:0,wait_work:1,work:2,complete_work:3}
 
 end
